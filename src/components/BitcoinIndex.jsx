de6cf0157466ecec89d61b89c7b2d910826087f1
Flynn <flynnwebdev@gmail.com>
2022-06-22 11:06:20 +1000

    Added dynamic currencies

--- src/components/BitcoinIndex.jsx
+++ src/components/BitcoinIndex.jsx
@@ -2,26 +2,16 @@ import { useState, useEffect } from "react";
 
 function BitcoinIndex({ currency }) {
   const [price, setPrice] = useState();
-  const [count, setCount] = useState(0);
 
   // Called on mount only
   useEffect(() => {
     fetch(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
       .then((res) => res.json())
       .then((data) => setPrice(data.bpi[currency].rate));
-  }, []);
-
-  useEffect(() => {
-    console.log("Called on any state or prop change");
-  });
-
-  useEffect(() => {
-    console.log("Called when count changes only");
-  }, [count]);
+  }, [currency]);
 
   return (
     <>
-      <h1>Bitcoin Index</h1>
       {price ? (
         <p>
           Current Price: {currency} {price}
@@ -29,7 +19,6 @@ function BitcoinIndex({ currency }) {
       ) : (
         <p>Loading...</p>
       )}
-      {/* <button onClick={() => setCount(count + 1)}>Click Me! {count}</button> */}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import BitcoinIndex from "./BitcoinIndex";
+import App from "./components/App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<BitcoinIndex currency="AUD" />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
